2a9c024ef005a0d3394345adbb126587dc5a5c1a
Егор <@DESKTOP-VF4U8Q2>
2024-10-28 06:24:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication21/ConsoleApplication21.cpp
@@ -0,0 +1,28 @@
+﻿#include <cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    float x, y, z;
+
+    cout << "Введите размеры бруска: ";
+    cin >> x >> y >> z; 
+
+    float cubes = floor((x / 5) * (y / 5) * (z / 5));
+    
+    cout << "Из этого бруска можно изготовить " << cubes << " кубиков.";
+
+    if (cubes >= 27)
+        cout << "Из них можно составить набор из 27 кубиков.";
+
+
+    else
+        cout << "Нельзя составить набор ";
+
+
+
+    
+}
+
